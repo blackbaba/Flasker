d1a0471e9ae730c4aa4ecf7e40bbaaa37b603a04
Amrinder Dhindsa <amrinder.dhindsa1@gmail.com>
2021-04-28 17:27:08 +1000

    updated requirements.txt

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SSL_REDIRECT = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
@@ -34,30 +35,47 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
     @classmethod
     def init_app(cls, app):
         Config.init_app(app)
-    
-    # Send emails to admins
-    import logging
-    from logging.handlers import SMTPHandler
-    credentials = None
-    secure = None
 
-    if getattr(cls, 'MAIL_USERNAME', None) is not None:
-        credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
-        if getattr(cls, 'MAIL_USE_TLS', None):
-            secure = ()
-    mail_handler = SMTPHandler(
-        mailhost = (cls.MAIL_SERVER, cls.MAIL_PORT),
-        fromaddr = cls.FLASKY_MAIL_SENDER,
-        toaddrs = [cls.FLASKY_ADMIN],
-        subject = cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
-        credentials = credentials,
-        secure = secure 
-    )
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+        # email errors to the administrators
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.FLASKY_MAIL_SENDER,
+            toaddrs=[cls.FLASKY_ADMIN],
+            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+class HerokuConfig(ProductionConfig):
+    SSL_REDIRECT = True if os.environ.get('DYNO') else False
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # Log to stderr
+        import logging
+        from logging import StreamHandler
+
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        # handle reverse proxy server headers
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
 
 config = {
     'development': DevelopmentConfig,
